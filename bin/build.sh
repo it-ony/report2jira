5e4b42fbd45805f21dab9f7e2e45c7a08bb0d99a
Tony Findeisen <github@it-ony.de>
2017-06-27 17:09:56 +0200

    fixed naming of build script

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR} && cd ..
 
-zip --exclude=*.DS_Store* --exclude=*bin* --exclude=*.git* --exclude=*.iml --exclude=*.afdesign --exclude=*.idea* --exclude=*node_modules* --exclude=LICENSE --exclude=README.md -r raygun2jira.zip .
+zip --exclude=*.DS_Store* --exclude=*bin* --exclude=*.git* --exclude=*.iml --exclude=*.afdesign --exclude=*.idea* --exclude=*node_modules* --exclude=LICENSE --exclude=README.md -r report2jira.zip .
 
 echo "";
-echo "raygun2jira.zip written";
+echo "report2jira.zip written";
 echo "";
 
 cd $PWD
